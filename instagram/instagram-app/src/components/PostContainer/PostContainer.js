71a6cc57398aac7e06bb4e09358c469a0e465d93
azinoVo <azinovo22@gmail.com>
2019-04-30 15:51:14 -0500

    added PropTypes

--- instagram/instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram/instagram-app/src/components/PostContainer/PostContainer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import CommentSection from '../CommentSection/CommentSection';
 
 
@@ -10,16 +11,36 @@ const PostContainer = props => {
                     <h2>{post.username}</h2>
                     <img src={post.thumbnailUrl} alt="thumbnail"/>
                     <img src={post.imageUrl} alt="profile"/>
-                    <p>{post.likes}, {post.timestamp}</p>
+                    <p>Likes: {post.likes}, Time: {post.timestamp}</p>
                     <div className='comment-section'>
                         {post.comments.map((comment,index)=>
-                        <p key={index}>{comment.username}: {comment.text}</p>
+                        <p key={index}><strong>{comment.username}</strong>: {comment.text}</p>
                         )}
                     </div>
                 </div>
             ))}
         </>
-    );    
+    );   
 }
 
+PostContainer.propTypes = {
+    objectDummy: PropTypes.arrayOf(
+        PropTypes.shape({
+            username: PropTypes.string,
+            thumbnailUrl: PropTypes.string,
+            imageUrl: PropTypes.string,
+            likes: PropTypes.number,
+            timestamp: PropTypes.string,
+            comments: PropTypes.arrayOf(
+                    PropTypes.shape({
+                        username: PropTypes.string,
+                        text: PropTypes.string
+                    }))
+            }))
+    }
+
+PostContainer.defaultProps = {
+    objectDummy: []
+      };
+      
 export default PostContainer;
